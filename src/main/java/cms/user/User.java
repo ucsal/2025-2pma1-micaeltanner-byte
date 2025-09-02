38c66e89b8d897a4fdcfe1da94cc210b8b8b6709
micaeltanner-byte <micael.tanner@ucsal.edu.br>
2025-09-02 20:11:49 -0300

    Create User.java
    Professor, fiz pelo Vscode, acabei tendo uns problemas, mas consegui fazer assim.

---
+++ src/main/java/cms/user/User.java
@@ -0,0 +1,44 @@
+package cms.user;
+
+public abstract class User {
+    protected int id;
+    protected String nome;
+    protected String email;
+    protected String password;
+
+
+    protected User(int id, String nome, String email, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+    }
+
+   
+    public int  getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    
+    public boolean checkPassword(String candidate) {
+        return password != null && password.equals(candidate);
+    }  
+
+    public static void main(String[] args) {
+        User u1 = new AdminUser(1, "Alice", "alice@example.com", "abc123");
+        User u2 = new EditorUser(2, "Bruno", "bruno@example.com", "abc456");
+        User u3 = new GuestUser(3, "Carla", "carla@example.com", "abc789");
+
+        boolean ok1 = u1.checkPassword("abc123"); 
+        boolean ok2 = u2.checkPassword("wrong");  
+        boolean ok3 = u3.checkPassword("abc789"); 
+
+        System.out.println("ok1: " + ok1); 
+        System.out.println("ok2: " + ok2); 
+        System.out.println("ok3: " + ok3); 
+
+        System.out.println(u1.getNome()); 
+        System.out.println(u3.getNome()); 
+    }
+}
